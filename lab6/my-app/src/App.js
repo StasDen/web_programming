458abbd10c6e95de9876bc83585e9e964ba20614
Stas Denysenko <lohopusik67@gmail.com>
2022-11-13 22:43:07 +0200

    Implemented lab9

--- lab6/my-app/src/App.js
+++ lab6/my-app/src/App.js
@@ -2,13 +2,45 @@ import "./App.css";
 import Navigation from "./containers/Navigation/Navigation";
 import Footer from "./containers/Footer/Footer";
 
-function App() {
-  return (
-    <div>
-      <Navigation />
-      <Footer />
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: "",
+    };
+  }
+
+  callApi() {
+    fetch("http://localhost:9000/api/courses")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callApi();
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>REST API</h1>
+        </header>
+        <p>{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <Navigation />
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
